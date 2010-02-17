119528f538b5811a7e5f0290603501ad38dca724
Volker Pacher <volker.pacher@gmail.com>
2010-02-17 19:05:18 +0000

    implemented further methods for xpay
    -private method xpay, handles writing xml to the socket and reads result from socket
    -private method add_certificate, adds certificate before any write to socket
    -private method load_certificate, loads certificate according to xpay.yml

--- lib/xpay.rb
+++ lib/xpay.rb
@@ -21,5 +21,29 @@ module Xpay
     def config
       @xpay_config
     end
+
+    private
+
+    def add_certificate(doc)
+      cer = doc.root.add_element("Certificate")
+      cer.text = Payment.cert_load
+      return doc
+    end
+    def cert_load
+      mycert = ''
+      File.open(@xpay_config['path_to_cert'], "r") { |f| mycert = f.read}
+      mycert.chomp
+    end
+
+    def xpay(request_block)
+      r_block = add_certificate(request_block)
+      a = TCPSocket.open("localhost",5000)
+      a.write(r_block.to_s)
+      res = a.read()
+      a.close
+      response_block = REXML::Document.new res[res.index("<ResponseBlock"), res.length]
+      action_code = REXML::XPath.first(response_block, "//Result").text.to_i
+      return response_block, action_code
+    end
   end
 end
