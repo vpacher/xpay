f10cac76012e2ec1d5d3d21948cacedd158b5468
Volker Pacher <volker.pacher@gmail.com>
2010-10-28 14:05:42 +0100

    added test for transactionquery and added create_request method to class

--- lib/xpay/transaction_query.rb
+++ lib/xpay/transaction_query.rb
@@ -4,9 +4,25 @@ module Xpay
     attr_accessor :transaction_reference, :order_reference, :site_reference
 
     def initialize(options={})
+      @request_xml = REXML::Document.new(Xpay.root_to_s)
       options.each do |key, value|
         self.send("#{key}=", value) if self.respond_to? key
       end
+      create_request
     end
+
+    private
+    def create_request
+      raise AttributeMissing.new "(2500) TransactionReference or OrderReference need to be present." if (transaction_reference.nil? && order_reference.nil?)
+      raise AttributeMissing.new "(2500) SiteReference must be present." if (site_reference.nil? && (REXML::XPath.first(@request_xml, "//SiteReference").text.blank? rescue true))
+      REXML::XPath.first(@request_xml, "//Request").attributes["Type"] = "TRANSACTIONQUERY"
+      ops = REXML::XPath.first(@request_xml, "//Operation")
+      ["TermUrl", "MerchantName"].each { |e| ops.delete_element e }
+      (ops.elements["SiteReference"] || ops.add_element("SiteReference")).text = self.site_reference if self.site_reference
+      (ops.elements["TransactionReference"] || ops.add_element("TransactionReference")).text = self.transaction_reference if self.transaction_reference
+      order = REXML::XPath.first(@request_xml, "//Operation")
+      (order.elements["OrderReference"] || order.add_element("OrderReference")).text = self.order_reference if self.order_reference
+    end
+
   end
 end
--- test/xpay/transactionquery_test.rb
+++ test/xpay/transactionquery_test.rb
@@ -2,10 +2,36 @@ require "test_helper"
 require 'xpay'
 
 class TransactionQueryTest < Test::Unit::TestCase
-
   context "an empty instance" do
+    should "raise attribute missing error" do
+      assert_raise(Xpay::AttributeMissing) { Xpay::TransactionQuery.new() }
+    end
+  end
+
+  context "an instance with missing site refence " do
     setup do
-      @t = Xpay::TransactionQuery.new()
+      ops = REXML::XPath.first(Xpay.root_xml, "//Operation")
+      ops.delete_element "SiteReference"
+    end
+    should "raise attribute missing error" do
+      assert_raise(Xpay::AttributeMissing) { Xpay::TransactionQuery.new(:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211") }
+    end
+  end
+  context "an instance with empty site refence " do
+    setup do
+      REXML::XPath.first(Xpay.root_xml, "//SiteReference").text = ""
+    end
+    should "raise attribute missing error" do
+      assert_raise(Xpay::AttributeMissing) { Xpay::TransactionQuery.new(:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211") }
+    end
+  end
+
+  context "a new instance" do
+    setup do
+      @t = Xpay::TransactionQuery.new({:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211", :site_reference => "site1234"})
+    end
+    should "have a the same xml as the module" do
+      assert_equal @t.request_xml.root.to_s, load_xml_string("transactionquery")
     end
 
     should "have attributes" do
