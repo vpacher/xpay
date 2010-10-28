df92cde1f1bd487e21d1542eb7add08bf5c90e16
Volker Pacher <volker.pacher@gmail.com>
2010-10-28 14:34:42 +0100

    added test for query response and class methods, added comments

--- lib/xpay/transaction_query.rb
+++ lib/xpay/transaction_query.rb
@@ -1,5 +1,9 @@
 module Xpay
 
+  # This class allows you to query the status of a transaction processed with Xpay
+  # the following attributes are required:
+  # either a TransactionReference or your own OrderReference from a previous transaction
+  # and your SiteReference if not already set in the config, you can also pass it to overwrite it for this transaction
   class TransactionQuery < Transaction
     attr_accessor :transaction_reference, :order_reference, :site_reference
 
@@ -11,7 +15,31 @@ module Xpay
       create_request
     end
 
+    def query
+      process()
+      return response_code
+    end
+
+    # The response_block is a hash and can have one of several values:
+    # the follwing values are always present after a transaction and can be queried to gain further details of the transaction:
+    #   * result_code:
+    #       0 for failure, check error_code for further details
+    #       1 transaction was succesful
+    #       2 transaction was denied
+    #   * transaction_reference
+    #   * transactionverifier
+    #   * transaction_time
+    #   * auth_code
+    #   * amount
+    #   * the order reference
+    def response_block
+      create_response_block
+    end
+
     private
+
+    # Write the xml document needed for processing, fill in elements need and delete unused ones from the root_xml
+    # raises an error if any necessary elements are missing
     def create_request
       raise AttributeMissing.new "(2500) TransactionReference or OrderReference need to be present." if (transaction_reference.nil? && order_reference.nil?)
       raise AttributeMissing.new "(2500) SiteReference must be present." if (site_reference.nil? && (REXML::XPath.first(@request_xml, "//SiteReference").text.blank? rescue true))
@@ -22,6 +50,19 @@ module Xpay
       (ops.elements["TransactionReference"] || ops.add_element("TransactionReference")).text = self.transaction_reference if self.transaction_reference
       order = REXML::XPath.first(@request_xml, "//Operation")
       (order.elements["OrderReference"] || order.add_element("OrderReference")).text = self.order_reference if self.order_reference
+    end
+
+    def create_response_block
+      @response_xml.is_a?(REXML::Document) ?
+              {
+                      :result_code => (REXML::XPath.first(@response_xml, "//Result").text.to_i rescue nil),
+                      :transaction_reference => (REXML::XPath.first(@response_xml, "//TransactionReference").text rescue nil),
+                      :transactionverifier => (REXML::XPath.first(@response_xml, "//TransactionVerifier").text rescue nil),
+                      :transaction_time => (REXML::XPath.first(@response_xml, "//TransactionCompletedTimestamp").text rescue nil),
+                      :auth_code => (REXML::XPath.first(@response_xml, "//AuthCode").text rescue nil),
+                      :amount => (REXML::XPath.first(@response_xml, "//Amount").text.to_i rescue nil),
+                      :order_reference => (REXML::XPath.first(@response_xml, "//OrderReference").text rescue nil),
+              } : {}
     end
 
   end
--- test/xpay/transactionquery_test.rb
+++ test/xpay/transactionquery_test.rb
@@ -13,6 +13,7 @@ class TransactionQueryTest < Test::Unit::TestCase
       ops = REXML::XPath.first(Xpay.root_xml, "//Operation")
       ops.delete_element "SiteReference"
     end
+
     should "raise attribute missing error" do
       assert_raise(Xpay::AttributeMissing) { Xpay::TransactionQuery.new(:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211") }
     end
@@ -21,6 +22,7 @@ class TransactionQueryTest < Test::Unit::TestCase
     setup do
       REXML::XPath.first(Xpay.root_xml, "//SiteReference").text = ""
     end
+
     should "raise attribute missing error" do
       assert_raise(Xpay::AttributeMissing) { Xpay::TransactionQuery.new(:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211") }
     end
@@ -30,8 +32,13 @@ class TransactionQueryTest < Test::Unit::TestCase
     setup do
       @t = Xpay::TransactionQuery.new({:transaction_reference => "17-9-1908322", :order_reference => "121-1010272211", :site_reference => "site1234"})
     end
+
     should "have a the same xml as the module" do
       assert_equal @t.request_xml.root.to_s, load_xml_string("transactionquery")
+    end
+
+    should "have an empty repsonse block" do
+      assert @t.response_block.empty?
     end
 
     should "have attributes" do
@@ -39,5 +46,27 @@ class TransactionQueryTest < Test::Unit::TestCase
       assert @t.respond_to? :order_reference
       assert @t.respond_to? :site_reference
     end
+
+    context "given a response block" do
+      setup do
+        Xpay::TransactionQuery.send(:public, *Xpay::TransactionQuery.private_instance_methods)
+        @t.send("response_xml=", load_xml("transactionquery_response"))
+      end
+
+      should "have a response block" do
+        assert !@t.response_block.empty?
+      end
+
+      should "have values" do
+        rb = @t.response_block
+        assert_equal "2-2-35117", rb[:transaction_reference]
+        assert_equal "AUTH CODE: ab123", rb[:auth_code]
+        assert_equal 1, rb[:result_code]
+        assert_equal "2004-08-31 16:17:03", rb[:transaction_time]
+        assert_equal "ApTtGHQ/WUQYRj", rb[:transactionverifier]
+        assert_equal "orderref0001", rb[:order_reference]
+      end
+    end
   end
+
 end
