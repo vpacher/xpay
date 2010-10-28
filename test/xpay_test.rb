9e9587820b5d707a3fbdd4d4b8dd797b682d9490
Volker Pacher <volker.pacher@gmail.com>
2010-10-28 16:51:16 +0100

    added test to assure new root_xml is created after config change via hash

--- test/xpay_test.rb
+++ test/xpay_test.rb
@@ -8,21 +8,29 @@ class XpayTest < Test::Unit::TestCase
     end
 
     should "have standard config xml" do
-      assert_kind_of(REXML::Document,@xpay.root_xml)
+      assert_kind_of(REXML::Document, @xpay.root_xml)
       assert_equal @xpay.config, OpenStruct.new(xpay_config("default"))
     end
     should "have xml document formed to xpay spec" do
       assert_equal load_xml_string("root"), @xpay.root_xml.root.to_s
     end
+
     should "load a new config from yaml file" do
-      assert @xpay.load_config(File.expand_path(File.dirname(__FILE__)+ '/fixtures'))
+      assert @xpay.load_config(File.expand_path(File.dirname(__FILE__) + '/fixtures'))
     end
 
-    context "with new configuration" do
+    context "with new configuration from yaml file" do
 
       should "have a new config" do
         assert_equal @xpay.config, OpenStruct.new(xpay_config("config_load_test"))
-        d {@xpay.root_xml.to_s}
+      end
+
+      should "have a new root_xml" do
+        assert_equal load_xml_string("config_load_test"), @xpay.root_xml.root.to_s
+      end
+
+      should "have config.port 6000" do
+        assert_equal 6000, @xpay.config.port
       end
 
       should "have an environment" do
@@ -37,7 +45,9 @@ class XpayTest < Test::Unit::TestCase
     should "load a new config from hash" do
       assert @xpay.set_config(xpay_config("default"))
       assert_equal @xpay.config, OpenStruct.new(xpay_config("default"))
+      assert_equal load_xml_string("default_config"), @xpay.root_xml.root.to_s
     end
 
   end
 end
+
